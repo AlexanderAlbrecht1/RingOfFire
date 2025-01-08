b700a32e7bf015b341e588c44f2e25a327cb8a90
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-01-08 13:27:45 +0100

    load and shuffle cardstack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../models/game';
 
 @Component({
   selector: 'app-game',
@@ -10,7 +11,17 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  game: Game = new Game();
 
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+
+  }
 
   takeCard() {
     this.pickCardAnimation = true;
